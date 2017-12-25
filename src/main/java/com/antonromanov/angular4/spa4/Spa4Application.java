4aa31bd02b51961ef76a8f2c46af703b16c2e2ed
Latinist <antonromanov@list.ru>
2017-12-25 16:15:24 +0300

    first upload

---
+++ src/main/java/com/antonromanov/angular4/spa4/Spa4Application.java
@@ -0,0 +1,12 @@
+package com.antonromanov.angular4.spa4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spa4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spa4Application.class, args);
+	}
+}
