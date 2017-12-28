dfc319dfdbbe78a37ee07c69100c4051018e2dbd
Anton Romanov <antonromanov@list.ru>
2017-12-28 16:51:45 +0300

    Configured for Tomcat Version

--- src/main/java/com/antonromanov/angular4/spa4/Spa4Application.java
+++ src/main/java/com/antonromanov/angular4/spa4/Spa4Application.java
@@ -2,11 +2,18 @@ package com.antonromanov.angular4.spa4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Spa4Application {
+public class Spa4Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Spa4Application.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Spa4Application.class, args);
 	}
 }
--- src/test/java/com/antonromanov/angular4/spa4/Spa4ApplicationTests.java
+++ src/test/java/com/antonromanov/angular4/spa4/Spa4ApplicationTests.java
@@ -1,16 +1,17 @@
 package com.antonromanov.angular4.spa4;
 
+/*
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest */
 public class Spa4ApplicationTests {
-
+/*
 	@Test
 	public void contextLoads() {
 	}
-
+*/
 }
