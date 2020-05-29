4c6e1e62064dcb9486b527d9a3775446533eed49
Latinist <antonromanov@list.ru>
2020-05-29 17:38:41 +0300

    Легкие исправления, чтобы проверить пересборку Дженкинса

--- src/main/java/com/antonromanov/angular4/spa4/controller/WebRestController.java
+++ src/main/java/com/antonromanov/angular4/spa4/controller/WebRestController.java
@@ -7,26 +7,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/rest/user")
+@RequestMapping("/rest")
 public class WebRestController {
 
 
     String json = "{\"id\":2488,\"content\":\"Hello!\"}";
-    /*
-    @RequestMapping(
-            value = "/hello",
-            method = RequestMethod.GET,
-            produces = "application/json"
-    )
-    public String hi(@RequestParam(value = "name") String name){
-        return name+" - TEST";
-    }
-
-   */
-
     @GetMapping(value="/hello")
     public String hi(@RequestParam(value = "name") String name){
-    //    return name+" - TEST";
         return json;
     }
 
